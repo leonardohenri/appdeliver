c8657a8d97a5f9a81a0ce8a3c84f6220e280eb01
unknown <leonardohenri2015@outlook.com>
2021-01-08 14:50:57 -0300

    initial visual

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import Counter from './Counter'
+import NavBAr from './NavBar';
+
 
 function App() {
   return (
     <div>
-  <Counter/>.
+  <NavBAr />
     </div>
   );
 }
---
+++ front-web/src/NavBar/index.tsx
@@ -0,0 +1,10 @@
+import './styles.css';
+import{ ReactComponent as Logo } from './logo.svg';
+function NavBAr(){
+    return(
+        <nav className="main-navbar">
+         <Logo />
+        </nav>
+    )
+}
+export default NavBAr;
